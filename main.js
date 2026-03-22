d51eff9f4531225d01bf0ca82414a7c7fd2ee131
Zoyah74 <zoyahshah74@gmail.com>
2026-03-21 20:35:48 -0700

    Update main.js

--- main.js
+++ main.js
@@ -383,6 +383,7 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
     restartBtn.addEventListener("click", () => {
+      isPaused = false;
       window.FingerprintApp.play();
       setPauseState();
       setDotGenerating();
